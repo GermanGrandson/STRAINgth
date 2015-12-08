ad33efb827f1826375b72ac5fafde5ec32ceeeb0
German Grandson <ganieto1@gmail.com>
2015-12-08 15:22:17 -0500

    some refactoring

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -11,19 +11,19 @@ class StrainsController < ApplicationController
      @lineage = lineage
     end
 
-
   def show
     puts params[:id]
     url = "http://api.leoqz.me/api/strains?symptom=" + params[:id]
     response = HTTParty.get(url)
-    json_data = JSON.parse(response.body)
-    @strains = json_data["data"]
+    # json_data = JSON.parse(response.body)
+    @strains = response["data"]
   end
 
   def plant
     url = "http://api.leoqz.me/api/strain/" + params[:ucpc]
     response = HTTParty.get(url)
-    json_data = JSON.parse(response.body)
-    @strains = json_data["data"]
+    # json_data = JSON.parse(response.body)
+    @strains = response["data"]
   end
+
 end
--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,16 +1,13 @@
 class SymptomsController < ApplicationController
   def index
-    
   end
 
-  #helper methods for getting list of strains
+  #Method for getting list of strains
   private
     def find_strains(symptom)
       url = "http://api.leoqz.me/api/strains?symptom=#{symptom}"
       response = HTTParty.get(url)
-      json_data = JSON.parse(response.body)
-      @strains = json_data["data"]
+      @strains = response["data"]
     end
-  helper_method :find_strains
 
 end
